a667b28df4bd883a8be358284955437f3b5c3af5
unknown <Administrateur@LAB.LOCAL>
2025-04-23 17:34:33 +0200

    refacto: v1 fonctionel

--- Brouillon/ADadmin.ps1
+++ Brouillon/ADadmin.ps1
@@ -3,54 +3,56 @@ class ADadmin {
     [string]$ouCreationPath
     [string]$groupPath
     
-    ADadmin([object[]]$file,[string]$ouCreationPath, [string]$groupPath) {
+    ADadmin([object[]]$file, [string]$ouCreationPath, [string]$groupPath) {
         if (-not (Test-Path $file )) {
             throw "Le chemin n'existe pas" 
         }
         elseif (-not (Get-ADOrganizationalUnit -Identity $ouCreationPath -ErrorAction SilentlyContinue)) {
-            throw "Le chemin de création de l'OU n'existe pas"
+            throw "Le chemin de creation de l'OU n'existe pas"
         }
         $this.file = Import-Csv $file
         $this.ouCreationPath = $ouCreationPath
-        Write-Host $this.ouCreationPath
         $this.groupPath = $groupPath
-        Write-Host "Ok constructeur"
-
-        # === DEBUG ===
-        foreach ($ligne in $this.file) {
-            Write-Host "Prénom : $($ligne.Login)"
-        }
     }
 
-    [void] creationUser([string]$prenom, [string]$nom, [string]$login, [string]$password, [string]$service,[string]$ouPath) {
+    [void] creationUser([string]$prenom, [string]$nom, [string]$login, [string]$password, [string]$service, [string]$ouCreationPath) {
         $user = Get-ADUser -Filter { Name -eq $login }
-        if (-not ($user)) {
-            New-ADUser -Name $login -SamAccountName $login -GivenName $prenom -Surname $nom -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true -Path $ouPath
-            Write-Host "L'utilisateur `$prenom` `$nom` a été crée" -ForegroundColor Green
-            $this.ajoutGroupUser($login,$service)
+        try {
+            if (-not ($user)) {
+                New-ADUser `
+                    -Name $login `
+                    -SamAccountName $login `
+                    -GivenName $prenom `
+                    -Surname $nom `
+                    -UserPrincipalName "$($login)@labcorp.local" `
+                    -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+                    -Enabled $true `
+                    -Path $ouCreationPath 
+                Write-Host "L'utilisateur $prenom $nom a ete cree" -ForegroundColor Green
+                Write-Host "L'utilisateur $prenom $nom a bien ete ajoute a l'OU $service" -ForegroundColor Green
+                $this.ajoutGroupUser($login, $service)
+            }
         }
-        else {
-            throw "L'utilisateur `$prenom` `$nom` n'a pas pu etre crée"
+        catch {
+            Write-Host "L'utilisateur $prenom $nom n'a pas pu etre cree"
         }
     }
 
     [void] creationOU([string]$service) {
-        $ou = Get-ADOrganizationalUnit -Identity "$this.ouCreationPath"
+        $ou = Get-ADOrganizationalUnit -Filter "Name -eq '$($service)'" -ErrorAction SilentlyContinue
         if (-not($ou)) {
-            New-ADOrganizationalUnit -Name $service -Path $this.ouCreationPath
-            Write-Host "ok"
-        }else{
-            Write-Host "pas ok"
+            New-ADOrganizationalUnit -Name $service -ProtectedFromAccidentalDeletion $false -Path $this.ouCreationPath
         }
     }
 
     [void] supprimerOu([string]$service) {
-        $ou = Get-ADOrganizationalUnit -Identity $this.ouCreationPath -ErrorAction SilentlyContinue
+        $ou = Get-ADOrganizationalUnit -Filter "Name -eq '$($service)'" -ErrorAction SilentlyContinue
         if ($ou) {
-            Remove-ADOrganizationalUnit -Identity $service -Confirm:$false
-            Write-Host "L'OU `$service` a bien été suppprimé" -ForegroundColor Green
-        } else {
-            throw "L'OU `$service` n'a pas été trouvé"
+            Remove-ADOrganizationalUnit -Identity $ou -Recursive -Confirm:$false
+            Write-Host "L'OU $service a bien ete supprime" -ForegroundColor Green
+        }
+        else {
+            Write-Host "L'OU $service n'a pas ete trouve" -ForegroundColor Yellow
 
         }
     }
@@ -68,10 +70,10 @@ class ADadmin {
         $group = Get-ADGroup -Identity $service -ErrorAction SilentlyContinue
         if ($group) {
             Add-ADGroupMember -Identity $service -Members $login 
-            Write-Host "`$login` a bien été ajouté au groupe `$service` "
+            Write-Host "$login a bien ete ajoute au groupe $service " -ForegroundColor Green
         }
         else {
-            throw "Le groupe `$service` n'a pas été trouvé"
+            throw "Le groupe $service n'a pas ete trouve" 
         }
         
     }
@@ -79,21 +81,30 @@ class ADadmin {
     [void] supprimerGroup([string]$service) {
         $group = Get-ADGroup -Filter { Name -eq $service } -ErrorAction SilentlyContinue
         if ($group) {
-            Remove-ADGroup -Identity $service -Confirm $true
-            Write-Host("Le groupe `$service` a bien été suppprimé") -ForegroundColor Green
+            Remove-ADGroup -Identity $service -Confirm:$false
+            Write-Host("Le groupe $service a bien ete supprime") -ForegroundColor Green
         }
         else {
-            throw "Le groupe `$service` n'a pas été trouvé"
+            Write-Host "Le groupe $service n'a pas ete trouve" -ForegroundColor Yellow
+        }
+    }
+
+    [void] supprimerTousLesGroups(){
+        foreach($u in $this.file){
+            $this.supprimerGroup($u.Service)
+        }
+    }
+
+    [void] supprimerToutesLesOU(){
+        foreach($u in $this.file){
+            $this.supprimerOu($u.Service)
         }
     }
 
     [void] creationPlusieursUsers() {
         foreach ($u in $this.file) {
-            Write-Host "Traitement de l'utilisateur : $($u.Prenom) $($u.Nom) - Service : $($u.Service)"
-            Write-Host "$($u.Service)"
             $this.creationOU($u.Service)
-            $ouPath = Get-ADOrganizationalUnit -Filter * 
-            Write-Host "$ouPath"
+            $ouPath = Get-ADOrganizationalUnit -Filter "Name -eq '$($u.Service)'"
             $this.creationGroup($u.Service)
             $this.creationUser($u.Prenom, $u.Nom, $u.Login, $u.Password, $u.Service, $ouPath)
         }
@@ -101,8 +112,10 @@ class ADadmin {
 }
 
 #Execution du programme
-$filePath =  "C:\Scripts\Ps\test.csv" #read-host "Entrez le chemin du fichier csv qui contient les données des users"
-$ouCreationPath = "OU=UO,OU=Lab,DC=lab,DC=local"  #read-host "Entrez le chemin où seront crée les OU correpondant au services"
+$filePath = ".\test.csv" #read-host "Entrez le chemin du fichier csv qui contient les données des users"
+$ouCreationPath = "OU=OU,OU=LAB,DC=lab,DC=local"  #read-host "Entrez le chemin où seront crée les OU correpondant au services"
 $groupPath = "OU=Group,OU=Lab,DC=lab,DC=local" #read-host "Entrez le chemin où seront crée les groupes correspondant au services"
-$srvADDS = [ADadmin]::new($filePath,$ouCreationPath,$groupPath)
+$srvADDS = [ADadmin]::new($filePath, $ouCreationPath, $groupPath)
 $srvADDS.creationPlusieursUsers()
+#$srvADDS.supprimerTousLesGroups()
+#$srvADDS.supprimerToutesLesOU()
