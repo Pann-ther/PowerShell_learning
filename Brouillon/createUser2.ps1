d5541fad7af7a60a1ebbaf43e814bcec2cf3f6aa
Pann-ther <projet.chebbah@gmail.com>
2025-06-19 02:14:27 +0200

    feat: ajout commentaire code

--- Brouillon/createUser2.ps1
+++ Brouillon/createUser2.ps1
@@ -1,142 +1,295 @@
-﻿class User {
-    [string]$prenom
-    [string]$nom 
-    [string]$cn
-    [string]$login 
-    [string]$pswd 
-    [string]$groupe
-    [bool]$enabled
-    [bool]$exist
-    [string]$path
-    [string]$log
+﻿class ADadmin {
+    [object[]]$file
+    [string]$ouCreationPath
+    [string]$ouGroupPath
+    
+    # Constructeur
+    ADadmin([object[]]$file, [string]$ouCreationPath, [string]$ouGroupPath) {
+        if (-not (Test-Path $file )) {}
+        if (-not (Get-ADOrganizationalUnit -Identity $ouCreationPath )) {}
+        if (-not (Get-ADOrganizationalUnit -Identity $ouGroupPath )) {}
+        $this.file = Import-Csv $file
+        $this.ouCreationPath = $ouCreationPath
+        $this.ouGroupPath = $ouGroupPath
+    }
 
 
-    User([string]$prenom, [string]$nom, [string]$pswd,[string]$groupe, [string]$path, [string]$log){
-        $this.prenom = $prenom
-        $this.nom = $nom
-        $this.cn = "$($this.prenom) $($this.nom)"
-        $this.login = $($this.prenom.Substring(0,1)+$this.nom).ToLower()
-        $this.pswd = $pswd
-        $this.groupe = $groupe
-        $this.path = $path
-        $this.log = $log
-
-        #Verifie si un user a deja le meme login
-        $index = 1
-        while (Get-ADUser -Filter {SamAccountName -eq $this.login}) {
-            $this.login = $this.login+$index
-            $this.cn = $this.cn+$index
-            $index++
+    # Gestion du programme
+    # Validation des chemins
+    [bool] validationPathCsv($pathCsv) {
+        if ((Test-Path $pathCsv) -and ([System.IO.Path]::GetExtension($pathCsv) -eq ".csv")) {
+            return $true
+        }
+        else {
+            return $false
         }
     }
 
-    [void]addLog([string]$message){
-        $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        "$date - $message" | Out-File -FilePath $this.log  -Append -Encoding utf8
-    }
-
-    [void]createOU(){
-        $ou = Read-Host "Entrez le nom de l'OU"
-        $pathOU = Read-Host "Entrez le chemin de l'OU"
-
-        if(Get-ADOrganizationalUnit -Filter {Name -eq $ou}){
-            Write-Host "L'OU existe deja"
-        }else{
-            try {
-                New-ADOrganizationalUnit -Name $ou -Path $pathOU -ErrorAction Stop
-                $this.addLog("INFO: l'OU $ou a ete cree avec succes") 
-            }
-            catch {
-                $this.addLog("ERROR: l'OU $ou n'a pas pu etre cree, veuillez rentrez un chemin valide ") 
-            }
+    [bool] validationPathCreationOU($pathCreationOU) {
+        $path = Get-ADOrganizationalUnit -Identity $pathCreationOU -ErrorAction SilentlyContinue
+        if ($path) {
+            return $true
+        }
+        else {
+            return $false
         }
     }
 
-    [void]userExists(){
-        try{
-            Get-ADUser -Identity $this.login -ErrorAction Stop
-            $this.exist = $true
-        }catch{
-            $this.exist = $false
+    [bool] validationPathCreationGroupe($pathCreationGroupe) {
+        $path = Get-ADGroup -Identity $pathCreationGroupe -ErrorAction SilentlyContinue
+        if ($path) {
+            return $true
+        }
+        else {
+            return $false
         }
     }
 
-    [void]createUser(){
-        $this.userExists()
-        if(-not $this.exist){
-            if(Get-ADOrganizationalUnit -Filter {distinguishedName -eq $this.path}){
-                try{
-                    New-ADUser -GivenName $this.prenom -Surname $this.nom -Name $this.cn -SamAccountName $this.login -path $this.path -AccountPassword (ConvertTo-SecureString $this.pswd -AsPlainText -Force) -ErrorAction Stop
-                    $this.addLog("INFO: l'utilisateur $($this.login) a ete cree avec succes")
-                }catch{
-                    $this.addLog("ERROR: l'utilisateur $($this.login) n'a pas pu etre cree $($_.Exception.Message)") 
-                }      
-            }else{
-                $this.addLog("ERROR: l'OU n'existe pas, l'utilisateur n'a pas ete cree. Entrez le chemin d'un OU existant") 
-            }
-        } else{
-            $this.addLog("EEROR: l'utilisateur $($this.login) est deja existant") 
-        }
+    # Affichage des menus
+    [int] menuConfigurationChemins() {
+        Write-Host "-------Configuration des chemins-------"
+        Write-Host "1. Chemin du fichier csv"
+        Write-Host "2. Chemin de creation des OU"
+        Write-Host "3. Chemin de creation des Groupes"
+        Write-Host "4. Retour au menu d'action"
+        $choix = [int](Read-Host "Votre choix")
+        return $choix
+    }
+    
+    [int] menuAction() {
+        Write-Host "-------Action-------"
+        Write-Host "1. Creation automatique de l'arborescence"
+        Write-Host "2. Supprimer la arborescence"
+        Write-Host "3. Configuration des chemins"
+        Write-Host "4. Quitter le programme"
+        $choix = [int](Read-Host "Votre choix")
+        return $choix
     }
 
-    [void]removeUser(){
-        try{
-            Remove-ADUser -Identity $this.login -Confirm:$false -ErrorAction Stop
-            $this.addLog("INFO: l'utilisateur ete supprime avec succes") 
-        }catch{
-            $this.addLog("ERROR: l'utilisateur $($this.login) n'a pas ete trouve, la suppression ne s'est pas effectuee") 
-        }
-    }
-
-    [void]enabledUser(){
-        $this.enabled = $true
-        try{
-            Set-ADUser -Identity $this.prenom -Enabled $true -ErrorAction Stop
-        }catch{
-            $this.addLog("ERROR: l'utilisateur $($this.login) n'a pas ete trouve")
-        }
-    }
-
-    [void]disabledUser(){
-        $this.enabled = $false
-        try{
-            Set-ADUser -Identity $this.prenom -Enabled $false -ErrorAction Stop
-        }catch{
-            $this.addLog("ERROR: l'utilisateur $($this.login) n'a pas ete trouve") 
-        }
-    }
-
-    [void]addGroupUser(){
-        if(-not (Get-ADGroupMember -Identity $this.groupe | Where-Object {$_.SamAccountName -eq $this.login})){
-            if(Get-ADGroup -Filter {name -eq $this.groupe}){
-                try{
-                    Add-ADGroupMember -Identity $this.groupe -Members $this.login
-                    $this.addLog("INFO: Utilisateur $($this.login) a ete ajoute avec succes dans le groupe $($this.groupe)")
-                }catch{
-                    $this.addLog("ERROR: l'utilisateur $($this.login) n'a pas pu etre ajoute au groupe") 
+    # Traitement des choix utlisateurs
+    [void] choixConfigurtion() {
+        while ($true) {
+            $choix = $this.menuConfigurationChemins()
+            Start-Sleep -Seconds 1
+            Clear-Host
+            switch ($choix) {
+                1 {
+                    while ($true) {
+                        $path = Read-Host "Entrez le chemin du fichier CSV"
+                        if ($this.validationPathCsv($path)) {
+                            $this.file = Import-Csv $path
+                            Write-Host "Le chemin est valide"
+                            break
+                        }
+                        else {
+                            Write-Host "Le chemin du fichier n'est pas valide ou le fichier n'est pas un .csv" -ForegroundColor Red
+                        }
+                    }
+                    Start-Sleep -Seconds 1
+                    Clear-Host
                 }
-            }else{
-                $this.addLog("ERROR: le groupe $($this.groupe) n'existe pas") 
+                2 {
+                    while ($true) {
+                        $path = Read-Host "Entrez le chemin de l'OU pour la creation des OU"
+                        if ($this.validationPathCreationOU($path)) {
+                            $this.ouCreationPath = $path
+                            Write-Host "Le chemin est valide"
+                            break
+                        }
+                        else {
+                            Write-Host "Le chemin de l'OU n'est pas valide" -ForegroundColor Red
+                        }
+                    }
+                    Start-Sleep -Seconds 1
+                    Clear-Host
+                }
+                3 {
+                    while ($true) {
+                        $path = Read-Host "Entrez le chemin de l'OU pour la creation des OU"
+                        if ($this.validationPathCreationGroupe($path)) {
+                            $this.ouGroupPath = $path
+                            Write-Host "Le chemin est valide"
+                            break
+                        }
+                        else {
+                            Write-Host "Le chemin de l'OU n'est pas valide" -ForegroundColor Red
+                        }
+                    }
+                    Start-Sleep -Seconds 1
+                    Clear-Host
+                }
+                4 {
+                    $this.choixAction()
+                }
+                Default {
+                    Write-Host "Faites un choix valide: un entier entre 1 à 4"  -ForegroundColor Red
+                    Start-Sleep -Seconds 2
+                    Clear-Host
+                }
             }
-        }else{
-            $this.addLog("ERROR: l'utilisateur $($this.login) fait deja partie du groupe $($this.groupe)") 
+        }
+    }
+
+    [void] choixAction() {
+        while ($true) {
+            $choix = $this.menuAction()
+            Start-Sleep -Seconds 1
+            Clear-Host
+            switch ($choix) {
+                1 {
+                    $this.creationArborescence()
+                    Start-Sleep -Seconds 5
+                    Clear-Host
+                }
+                2 {
+                    $this.suppressionArborescence() 
+                    Start-Sleep -Seconds 5
+                    Clear-Host
+                }
+                3 {
+                    $this.choixConfigurtion() 
+                    Start-Sleep -Seconds 5
+                    Clear-Host
+                }
+                4 {
+                    Write-Host "Vous allez quittez le programme"
+                    Start-Sleep -Seconds 5
+                    Clear-Host
+                    exit 
+                }
+                Default {
+                    Write-Host "Faites un choix valide: un entier entre 1 à 4"  -ForegroundColor Red
+                    Start-Sleep -Seconds 2
+                    Clear-Host
+                }
+            }
+        }
+    }
+
+    # Gestion des Users
+    [void] creationUser([string]$prenom, [string]$nom, [string]$login, [string]$password, [string]$service, [string]$ouCreationPath) {
+        $user = Get-ADUser -Filter { Name -eq $login }
+        try {
+            # Test si le User n'existe pas
+            if (-not ($user)) {
+                New-ADUser `
+                    -Name $login `
+                    -SamAccountName $login `
+                    -GivenName $prenom `
+                    -Surname $nom `
+                    -UserPrincipalName "$($login)@labcorp.local" `
+                    -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+                    -Enabled $true `
+                    -Path $ouCreationPath 
+                Write-Host "L'utilisateur $prenom $nom a ete cree" -ForegroundColor Green
+                Write-Host "L'utilisateur $prenom $nom a bien ete ajoute a l'OU $service" -ForegroundColor Green
+                $this.ajoutGroupUser($login, $service)
+            }
+            else {
+                Write-Host "L'utilisateur $prenom $nom est deja existant" -ForegroundColor Yellow
+            }
+        }
+        catch {
+            Write-Host "L'utilisateur $prenom $nom n'a pas pu etre cree"
+        }
+    }
+
+    [void] ajoutGroupUser([string]$login, [string]$service) {
+        $group = Get-ADGroup -Identity $service -ErrorAction SilentlyContinue
+        # Test si le groupe existe
+        if ($group) {
+            Add-ADGroupMember -Identity $service -Members $login 
+            Write-Host "$login a bien ete ajoute au groupe $service " -ForegroundColor Green
+        }
+        else {
+            throw "Le groupe $service n'a pas ete trouve" 
+        }
+    }
+
+    [void] creationArborescence() {
+        # Parcours chaque ligne du fichier
+        foreach ($u in $this.file) {
+            $this.creationOU($u.Service)
+            $ouPath = Get-ADOrganizationalUnit -Filter "Name -eq '$($u.Service)'"
+            $this.creationGroup($u.Service)
+            $this.creationUser($u.Prenom, $u.Nom, $u.Login, $u.Password, $u.Service, $ouPath)
+        }
+    }
+
+    [void] suppressionArborescence() {
+        $this.supprimerTousLesGroups()
+        $this.supprimerToutesLesOU()
+    }
+
+    # Gestion des OU
+    [void] creationOU([string]$service) {
+        $ou = Get-ADOrganizationalUnit -Filter "Name -eq '$($service)'" -ErrorAction SilentlyContinue
+        # Test si l'UO n'existe pas
+        if (-not($ou)) {
+            New-ADOrganizationalUnit -Name $service -ProtectedFromAccidentalDeletion $false -Path $this.ouCreationPath
+        }
+    }
+
+    [void] supprimerOu([string]$service) {
+        $ou = Get-ADOrganizationalUnit -Filter "Name -eq '$($service)'" -ErrorAction SilentlyContinue
+        # Test si l'UO n'existe pas
+        if ($ou) {
+            Remove-ADOrganizationalUnit -Identity $ou -Recursive -Confirm:$false
+            Write-Host "L'OU $service a bien ete supprime" -ForegroundColor Green
+        }
+        else {
+            Write-Host "L'OU $service n'a pas ete trouve" -ForegroundColor Yellow
+        }
+    }
+
+    [void] supprimerToutesLesOU() {
+        # Parcours chaque ligne du fichier
+        foreach ($u in $this.file) {
+            $this.supprimerOu($u.Service)
+        }
+    }
+
+    # Gestion des Groups
+    [void] creationGroup([string]$service) {
+        try {
+            $group = Get-ADGroup -Identity $service 
+        }
+        catch { # Si la recherche genere une erreur (echoue) on la capture et on crée le group
+            New-ADGroup -Name $service -GroupScope Global -GroupCategory Distribution -Path $this.ouGroupPath
+        }
+    }
+
+    [void] supprimerGroup([string]$service) {
+        $group = Get-ADGroup -Filter { Name -eq $service } -ErrorAction SilentlyContinue
+        # Test si le groupe existe
+        if ($group) {
+            Remove-ADGroup -Identity $service -Confirm:$false
+            Write-Host("Le groupe $service a bien ete supprime") -ForegroundColor Green
+        }
+        else {
+            Write-Host "Le groupe $service n'a pas ete trouve" -ForegroundColor Yellow
+        }
+    }
+
+    [void] supprimerTousLesGroups() {
+        foreach ($u in $this.file) {
+            $this.supprimerGroup($u.Service)
         }
     }
 }
 
-$file = Import-Csv "C:\Shares\Scripts_powershell\users.csv" -Delimiter ";"
-$logfile = "C:\Shares\Scripts_powershell\logCreateUser.txt"
-
-foreach($u in $file){
-    $prenom = $u.prenom
-    $nom = $u.nom
-    $pswd = $u.mdp
-    $groupe = $u.groupe
-    $path = $u.path
-
-    $user = [User]::new($prenom,$nom,$pswd,$groupe,$path,$logfile)
-    $user.createUser()
-    $user.addGroupUser()
-}
-
-
+#Execution du programme
+do{
+    try{
+        $filePath = read-host "Entrez le chemin du fichier csv qui contient les donnees des users"
+        $ouCreationPath = read-host "Entrez le chemin ou seront cree les OU correpondant au services"
+        $ouGroupPath = read-host "Entrez le chemin ou seront cree les groupes correspondant au services"
+        $srvADDS = [ADadmin]::new($filePath, $ouCreationPath, $ouGroupPath)
+        Write-Host "Les chemins sont valides" -ForegroundColor Green
+    } catch {
+        Write-Host "Chemins invalides" -ForegroundColor Red
+    }
+} while ($srvADDS -eq $null) #La boucle se reexcute jusqu'a ce que $srvADDS ai une valeur differente de $null (aucune valeur)
+Start-Sleep -Seconds 1
+Clear-Host
+$srvADDS.choixAction() 
